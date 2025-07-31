9372f6c391688125890bc524aecbf0100907bd8a
Eborm <bramwesel@gmail.com>
2025-07-31 21:51:16 +0200

    text can now be formated to select specific parts of the text to highlight instead of the full text if bracktes are in the text

--- version4/Program.cs
+++ version4/Program.cs
@@ -13,6 +13,7 @@ namespace version4
             util util = new util();
             menu menu = new menu();
             menutext menutext = new menutext(util, menu);
+            menutext.setmenutexttoself(menutext);
 
             var key = Console.ReadKey(false).Key;
             Console.WriteLine(key.ToString());
@@ -20,24 +21,25 @@ namespace version4
 
 
             menutext.addtext("test text 1");
-            menutext.addtext("no", "test2");
             menutext.addtext("test text 2");
+            menutext.addtext("no", "test2");
             menutext.addtext("test text 3");
-            menutext.addtext("yes", "runshit");
+            menutext.addtext("remove text [yes]", "runshit");
+            menutext.addtext("removed all text");
+            menutext.setshowntext(new List<int> { 1, 2, 3, 4, 5 });
             while (true)
             {
-                menutext.writetext(new List<int> { 1, 2, 3, 4, 5});
+                menutext.writetext();
             }
 
         }
-        public static void runshit()
+        public static void runshit(util _util, menu _menu, menutext _menutext)
         {
             Console.Clear();
-            Console.WriteLine("runshit");
-            Console.ReadKey();
+            _menutext.setshowntext(new List<int> { 6});
         }
 
-        public static void test2()
+        public static void test2(util _util, menu _menu, menutext _menutext)
         {
             Console.Clear();
             Console.WriteLine("test 2 /no");
--- version4/classes/menutext.cs
+++ version4/classes/menutext.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace version4.classes
 {
@@ -11,13 +13,19 @@ namespace version4.classes
     {
         public util _util { get; set; }
         public menu _menu { get; set; }
-        private Dictionary<int, Tuple<string, string>> textdictionary = new Dictionary<int, Tuple<string, string> > { { 0, new Tuple<string, string>("don't use 0 i geuss", "-") } };
+        private Dictionary<int, Tuple<string, string>> textdictionary = new Dictionary<int, Tuple<string, string> > { { 0, new Tuple<string, string>("", "-") } };
         private int textcount = 0;
         private int selectedText = 0;
+        public List<int> writethistext = new List<int>();
+        menutext _menutext { get; set; }
         public menutext(util util, menu menu)
         {
             _util = util;
             _menu = menu;
+        }
+        public void setmenutexttoself(menutext menutext)
+        {
+            _menutext = menutext;
         }
 
         public void addtext(string text, string test)
@@ -31,7 +39,19 @@ namespace version4.classes
             textdictionary.Add(textcount, new Tuple<string, string>(text, "-"));
         }
 
-        public void writetext(List<int> writethistext)
+        public void removealltext()
+        {
+            textdictionary = new Dictionary<int, Tuple<string, string>> { { 0, new Tuple<string, string>("", "-") } };
+        }
+
+        public void setshowntext(List<int> newlist)
+        {
+            writethistext = newlist;
+            writethistext.Insert(0, 0);
+        }
+
+
+        public void writetext()
         {
             selectedText = Math.Clamp(selectedText, writethistext[0], writethistext[writethistext.Count - 1]);
             Console.Clear();
@@ -39,20 +59,58 @@ namespace version4.classes
             {
                 if (writetextkey == selectedText )
                 {
-                    _util.writelineselectedtext(textdictionary[writetextkey].Item1);
+                    _menutext.writeformatedtext(textdictionary[writetextkey].Item1);
                 }
-                else { Console.WriteLine(textdictionary[writetextkey].Item1); }
+                else { _menutext.writeunselectedtext(textdictionary[writetextkey].Item1); }
             }
             int input = _menu.detect_direction(_util);
-            if (input == 2) { typeof(version4.Program).GetMethod(textdictionary[selectedText].Item2)?.Invoke(null, null); }
+            object[] parm = { _util, _menu, _menutext };
+            if (input == 2) { typeof(version4.Program).GetMethod(textdictionary[selectedText].Item2)?.Invoke(null, parm); }
 
             else
             {
-                selectedText += input;
                 try
                 {
-                    if (input == 1) { while (textdictionary[selectedText].Item2 == "-") { selectedText++; } }
-                    else { while (textdictionary[selectedText].Item2 == "-") { selectedText--; } }
+                    if (input == 1 && selectedText == writethistext[writethistext.Count-1])
+                    {
+                        selectedText = 0;
+                    }
+                    else if (input == -1 && selectedText == writethistext[0])
+                    {
+                        selectedText = writethistext[writethistext.Count - 1];
+                    }
+                    if (input == 1)
+                    {
+                        selectedText++;
+                        while (textdictionary[selectedText].Item2 == "-")
+                        {
+                            {
+                                if (selectedText == writethistext[writethistext.Count - 1])
+                                {
+                                    selectedText = writethistext[0];
+                                }
+                                else
+                                {
+                                    selectedText++;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        selectedText--;
+                        while (textdictionary[selectedText].Item2 == "-")
+                        {
+                            if (selectedText == 0)
+                            {
+                                selectedText = writethistext[writethistext.Count - 1];
+                            }
+                            else
+                            {
+                                selectedText--;
+                            }
+                        }
+                    }
                 }
                 catch
                 {
@@ -60,6 +118,50 @@ namespace version4.classes
                 }
                 selectedText = Math.Clamp(selectedText, writethistext[0], writethistext[writethistext.Count - 1]);
             }
+        }
+    
+        public void writeformatedtext(string text)
+        {
+            List<char> splittext = text.ToList();
+            if (string.Join("", splittext).Contains("["))
+            {
+                foreach (char c in splittext)
+                {
+                    if (c == '[')
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    else if (c ==  ']') { Console.ResetColor(); }
+                    else
+                    { Console.Write(c); }
+                }
+                Console.WriteLine();
+            }
+            else { writelineselectedtext(text); }
+        }
+
+        public void writeunselectedtext(string text)
+        {
+            List<char> splittext = text.ToList();
+            foreach (char c in splittext)
+            {
+                if (c == '[')
+                {
+                    continue;
+                }
+                else if (c == ']') { continue; }
+                else { Console.Write(c); }
+            }
+            Console.WriteLine();
+        }
+    
+    public void writelineselectedtext(string text)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(text);
+            Console.ResetColor();
         }
     }
 }
